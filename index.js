9c5f613368e6ca9f11644db1278b92519839bcc7
wfreelifestyle <408230323@gms.tku.edu.tw>
2020-11-14 20:44:59 +0800

    Initial commit, midterm project

---
+++ index.js
@@ -0,0 +1,12 @@
+function change(index) {
+    //const p = document.getElementById('show');
+    const p = document.querySelector('#show');
+    console.log('p', p);
+    switch (index) {
+      case 1:
+        p.innerHTML = '<iframe width=100% height=100% src="./1.Class demo 每週內容/w01-intro_pokemon/intro.html">';
+  
+        break;
+    }
+  }
+  
